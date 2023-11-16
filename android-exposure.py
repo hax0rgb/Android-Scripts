44ef5f0c2c4b2869f7f50b9ab7ec9bc7bf51e682
Gaurang Bhatnagar <gaurang8492@gmail.com>
2023-11-16 12:47:04 +0530

    Update android-exposure.py

--- android-exposure.py
+++ android-exposure.py
@@ -4,14 +4,12 @@ import os
 from colorama import Fore, Style, init
 import glob
 from collections import defaultdict
-from lxml import etree as ET
 
 init(autoreset=True)
 
 class AndroidManifestAnalyzer:
     def __init__(self, manifest_path):
-        parser = ET.XMLParser(recover=True)  # Use a more robust parser
-        self.tree = ET.parse(manifest_path, parser)
+        self.tree = ET.parse(manifest_path)
         self.root = self.tree.getroot()
 
     def get_exported_activities(self):
@@ -21,7 +19,7 @@ class AndroidManifestAnalyzer:
             exported = activity.get('{http://schemas.android.com/apk/res/android}exported')
             enabled = activity.get('{http://schemas.android.com/apk/res/android}enabled', "true")
             intent_filters = activity.findall(".//intent-filter", namespaces=ns)
-            if (exported == "true" or (exported is None and intent_filters)) and enabled == "true":
+            if (exported == "true" or (exported is None and intent_filters)) and enabled != "false":
                 name = activity.get('{http://schemas.android.com/apk/res/android}name')
                 activities.append(name)
         return activities
@@ -32,7 +30,7 @@ class AndroidManifestAnalyzer:
         for provider in self.root.findall(".//provider", namespaces=ns):
             exported = provider.get('{http://schemas.android.com/apk/res/android}exported')
             enabled = provider.get('{http://schemas.android.com/apk/res/android}enabled', "true")
-            if exported == "true" and enabled == "true":
+            if exported == "true" and enabled != "false":
                 name = provider.get('{http://schemas.android.com/apk/res/android}name')
                 providers.append(name)
         return providers
@@ -43,7 +41,7 @@ class AndroidManifestAnalyzer:
         for service in self.root.findall(".//service", namespaces=ns):
             exported = service.get('{http://schemas.android.com/apk/res/android}exported')
             enabled = service.get('{http://schemas.android.com/apk/res/android}enabled', "true")
-            if exported == "true" and enabled == "true":
+            if exported == "true" and enabled != "false":
                 name = service.get('{http://schemas.android.com/apk/res/android}name')
                 services.append(name)
         return services
@@ -54,7 +52,7 @@ class AndroidManifestAnalyzer:
         for receiver in self.root.findall(".//receiver", namespaces=ns):
             exported = receiver.get('{http://schemas.android.com/apk/res/android}exported')
             enabled = receiver.get('{http://schemas.android.com/apk/res/android}enabled', "true")
-            if exported == "true" and enabled == "true":
+            if exported == "true" and enabled != "false":
                 name = receiver.get('{http://schemas.android.com/apk/res/android}name')
                 receivers.append(name)
         return receivers
